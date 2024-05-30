b7cc6e4ae0f6bc5a454ec60b6b40cfa5f0d52ad4
min.byun <leaftotoro@gmail.com>
2024-05-30 15:05:20 +0900

    12

--- Sample-Test1/baseball_test.cpp
+++ Sample-Test1/baseball_test.cpp
@@ -1,13 +1,11 @@
 #include "pch.h"
 #include "../BaseballProj/baseball.cpp"
+#include <stdexcept>
 
-TEST(BaseballGame, TryGameTest) {
-  EXPECT_EQ(1, 1);
-<<<<<<< HEAD
+using namespace std;
+
+TEST(BaseballGame, ThrowExceptionWheenInputLengthIsUnmatched) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
 }
-=======
-  EXPECT_EQ(1, 1);
-    EXPECT_EQ(1, 1);
-    EXPECT_EQ(1, 1);
-}
->>>>>>> 901192011b20f3e7a2ac4c8f3c3954d38ec6d60a
+
