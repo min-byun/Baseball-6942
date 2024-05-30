5edea6984a843ca9f3c838923929b769889e09e6
min.byun <leaftotoro@gmail.com>
2024-05-30 15:09:31 +0900

    add invalid_argument

--- Sample-Test1/baseball_test.cpp
+++ Sample-Test1/baseball_test.cpp
@@ -4,8 +4,12 @@
 
 using namespace std;
 
-TEST(BaseballGame, ThrowExceptionWheenInputLengthIsUnmatched) {
+TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
 	Baseball game;
 	EXPECT_THROW(game.guess(string("12")), length_error);
+}
+TEST(BaseballGame, ThrowExceptionWhenInvalidChar) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
 }
 
